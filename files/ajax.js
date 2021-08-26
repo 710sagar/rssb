036a21b7905391574ae0f9a0b800102eda300f60
Sagar Gupta <sagar.gupta@novoinvent.com>
2021-08-26 15:59:30 +0530

    first commit

---
+++ files/ajax.js
@@ -0,0 +1,43 @@
+function ajaxCall(value){
+    var vaccineName = document.getElementById('vaccine_name').value;
+    var campDate = document.getElementById('campDate').value;
+    var startNo = document.getElementById('startNo').value;
+    var endNo = document.getElementById('endNo').value;
+    if(value == 'Generate Tokens'){
+        url = 'files/rssb_token.html?vaccineName=' + encodeURIComponent(vaccineName) + '&campDate=' + campDate + '&startNo=' + startNo + '&endNo=' + endNo;
+    }
+    else if(value == 'Generate Registration Slips'){
+        url = 'files/rssb_reg.html?vaccineName=' + encodeURIComponent(vaccineName) + '&campDate=' + campDate + '&startNo=' + startNo + '&endNo=' + endNo;
+    }
+    document.location.href = url;
+}
+function validateForm(id) {
+    let vaccine_name = document.forms["campDetails"]["vaccine_name"].value;
+    let campDate = document.forms["campDetails"]["campDate"].value;
+    let startNo = document.forms["campDetails"]["startNo"].value;
+    let endNo = document.forms["campDetails"]["endNo"].value;
+    if (vaccine_name == "0") {
+      alert("Please Select Vaccine Name");
+      return false;
+    }
+    else if (campDate == "") {
+        alert("Please Enter Date");
+        return false;
+    }
+    else if (startNo == "") {
+        alert("Please Enter Starting Serial Number");
+        return false;
+    }
+    else if (endNo == "") {
+        alert("Please Enter Last Serial Number");
+        return false;
+    }
+    else if (startNo > endNo){
+        alert("Starting serial no. cannot be less than or equal to last serial no.")
+    }
+    else{
+        ajaxCall(id.value);
+    }
+}
+
+
